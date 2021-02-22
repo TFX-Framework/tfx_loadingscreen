11be701c141f914bcac238bbf8761deae03900bf
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-02-22 15:03:48 -0700

    Update __resource.lua

--- tfx_loadingscreen/__resource.lua
+++ tfx_loadingscreen/__resource.lua
@@ -1,4 +1,10 @@
 server_script 'system/versioncheck.lua'
+
+name 'tfx_loadingscreen'
+author 'TheRealToxicDev'
+contact 'https://redirect.toxicdev.me/discord'
+version '2.0.0'
+
 loadscreen 'index.html'
 files ({
     'index.html'
